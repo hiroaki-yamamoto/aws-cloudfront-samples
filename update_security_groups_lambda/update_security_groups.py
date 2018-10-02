c69f6df36ef218f7b6ddc553e10a098b26905950
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-10-02 15:42:17 +0900

    Added IPv6 support + Refactored code

--- update_security_groups_lambda/update_security_groups.py
+++ update_security_groups_lambda/update_security_groups.py
@@ -2,212 +2,228 @@
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
 
 
     http://aws.amazon.com/apache2.0/
 
 
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+or in the "license" file accompanying this file. This file is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the License for the specific language governing permissions
+and limitations under the License.
 '''
 
 import boto3
-import hashlib
 import json
-import urllib2
+from urllib import request as req
 
-# Name of the service, as seen in the ip-groups.json file, to extract information for
+IPRANGE_URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+
+# Name of the service, as seen in the ip-groups.json file,
+# to extract information for
 SERVICE = "CLOUDFRONT"
-# Ports your application uses that need inbound permissions from the service for
-INGRESS_PORTS = { 'Http' : 80, 'Https': 443 }
+# Ports your application uses that need inbound
+# permissions from the service for
+INGRESS_PORTS = {'http': 80, 'https': 443}
 # Tags which identify the security groups you want to update
-SECURITY_GROUP_TAG_FOR_GLOBAL_HTTP = { 'Name': 'cloudfront_g', 'AutoUpdate': 'true', 'Protocol': 'http' }
-SECURITY_GROUP_TAG_FOR_GLOBAL_HTTPS = { 'Name': 'cloudfront_g', 'AutoUpdate': 'true', 'Protocol': 'https' }
-SECURITY_GROUP_TAG_FOR_REGION_HTTP = { 'Name': 'cloudfront_r', 'AutoUpdate': 'true', 'Protocol': 'http' }
-SECURITY_GROUP_TAG_FOR_REGION_HTTPS = { 'Name': 'cloudfront_r', 'AutoUpdate': 'true', 'Protocol': 'https' }
+SECURITY_GROUP_TAGS = {
+    "CloudFront_g HttpSecurityGroups": {
+        'Name': 'cloudfront',
+        'AutoUpdate': 'true',
+        'Protocol': 'http',
+        'Region': 'global',
+    },
+    "CloudFront_g HttpsSecurityGroups": {
+        'Name': 'cloudfront',
+        'AutoUpdate': 'true',
+        'Protocol': 'https',
+        'Region': 'global',
+    },
+    "CloudFront_r HttpSecurityGroups": {
+        'Name': 'cloudfront',
+        'AutoUpdate': 'true',
+        'Protocol': 'http',
+        'Region': 'region',
+    },
+    "CloudFront_r HttpsSecurityGroups": {
+        'Name': 'cloudfront',
+        'AutoUpdate': 'true',
+        'Protocol': 'https',
+        'Region': 'region',
+    }
+}
+
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-
     # Load the ip ranges from the url
-    ip_ranges = json.loads(get_ip_groups_json(message['url'], message['md5']))
+    ip_ranges = json.loads(get_ip_groups_json(IPRANGE_URL))
 
     # extract the service ranges
     global_cf_ranges = get_ranges_for_service(ip_ranges, SERVICE, "GLOBAL")
     region_cf_ranges = get_ranges_for_service(ip_ranges, SERVICE, "REGION")
-    ip_ranges = { "GLOBAL": global_cf_ranges, "REGION": region_cf_ranges }
+    ip_ranges = {
+        "GLOBAL": global_cf_ranges,
+        "REGION": region_cf_ranges
+    }
 
     # update the security groups
     result = update_security_groups(ip_ranges)
 
     return result
 
-def get_ip_groups_json(url, expected_hash):
+
+def get_ip_groups_json(url):
     print("Updating from " + url)
 
-    response = urllib2.urlopen(url)
+    response = req.urlopen(url)
     ip_json = response.read()
-
-    m = hashlib.md5()
-    m.update(ip_json)
-    hash = m.hexdigest()
-
-    if hash != expected_hash:
-        raise Exception('MD5 Mismatch: got ' + hash + ' expected ' + expected_hash)
 
     return ip_json
 
+
 def get_ranges_for_service(ranges, service, subset):
-    service_ranges = list()
+    service_ranges = {
+        "ipv4": [],
+        "ipv6": [],
+    }
+
+    def region_mask(prefix):
+        return {
+            "GLOBAL": subset == prefix['region'] and subset == "GLOBAL",
+            "REGION": subset != 'GLOBAL' and prefix['region'] != 'GLOBAL'
+        }
+
     for prefix in ranges['prefixes']:
-        if prefix['service'] == service and ((subset == prefix['region'] and subset == "GLOBAL") or (subset != 'GLOBAL' and prefix['region'] != 'GLOBAL')):
-            print('Found ' + service + ' region: ' + prefix['region'] + ' range: ' + prefix['ip_prefix'])
-            service_ranges.append(prefix['ip_prefix'])
+        if prefix['service'] == service and any(region_mask(prefix).values()):
+            print(('Found {} region: {} range: {}').format(
+                service, prefix['region'], prefix['ip_prefix']
+            ))
+            service_ranges["ipv4"].append(prefix['ip_prefix'])
+    for prefix in ranges['ipv6_prefixes']:
+        if prefix['service'] == service and any(region_mask(prefix).values()):
+            print(('Found {} region: {} range: {}').format(
+                service, prefix['region'], prefix['ipv6_prefix']
+            ))
+            service_ranges["ipv6"].append(prefix['ipv6_prefix'])
 
     return service_ranges
 
+
 def update_security_groups(new_ranges):
     client = boto3.client('ec2')
+    result = []
 
-    global_http_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_GLOBAL_HTTP)
-    global_https_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_GLOBAL_HTTPS)
-    region_http_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_REGION_HTTP)
-    region_https_group = get_security_groups_for_update(client, SECURITY_GROUP_TAG_FOR_REGION_HTTPS)
-
-    print ('Found ' + str(len(global_http_group)) + ' CloudFront_g HttpSecurityGroups to update')
-    print ('Found ' + str(len(global_https_group)) + ' CloudFront_g HttpsSecurityGroups to update')
-    print ('Found ' + str(len(region_http_group)) + ' CloudFront_r HttpSecurityGroups to update')
-    print ('Found ' + str(len(region_https_group)) + ' CloudFront_r HttpsSecurityGroups to update')
-
-    result = list()
-    global_http_updated = 0
-    global_https_updated = 0
-    region_http_updated = 0
-    region_https_updated = 0
-
-    for group in global_http_group:
-        if update_security_group(client, group, new_ranges["GLOBAL"], INGRESS_PORTS['Http']):
-            global_http_updated += 1
-            result.append('Updated ' + group['GroupId'])
-    for group in global_https_group:
-        if update_security_group(client, group, new_ranges["GLOBAL"], INGRESS_PORTS['Https']):
-            global_https_updated += 1
-            result.append('Updated ' + group['GroupId'])
-    for group in region_http_group:
-        if update_security_group(client, group, new_ranges["REGION"], INGRESS_PORTS['Http']):
-            region_http_updated += 1
-            result.append('Updated ' + group['GroupId'])
-    for group in region_https_group:
-        if update_security_group(client, group, new_ranges["REGION"], INGRESS_PORTS['Https']):
-            region_https_updated += 1
-            result.append('Updated ' + group['GroupId'])
-
-    result.append('Updated ' + str(global_http_updated) + ' of ' + str(len(global_http_group)) + ' CloudFront_g HttpSecurityGroups')
-    result.append('Updated ' + str(global_https_updated) + ' of ' + str(len(global_https_group)) + ' CloudFront_g HttpsSecurityGroups')
-    result.append('Updated ' + str(region_http_updated) + ' of ' + str(len(region_http_group)) + ' CloudFront_r HttpSecurityGroups')
-    result.append('Updated ' + str(region_https_updated) + ' of ' + str(len(region_https_group)) + ' CloudFront_r HttpsSecurityGroups')
+    for (name, tag) in SECURITY_GROUP_TAGS.items():
+        groups = get_security_groups_for_update(client, tag)
+        print(("Found {} {} to update").format(len(groups), name))
+        num_update = 0
+        for group in groups:
+            if update_security_group(
+                client, group, new_ranges[tag["Region"].upper()],
+                INGRESS_PORTS[tag["Protocol"]]
+            ):
+                num_update += 1
+                result.append(("Updated {}").format(group['GroupId']))
+            result.append(
+                ("Updated {} of {} {}").format(num_update, len(groups), name)
+            )
 
     return result
+
 
 def update_security_group(client, group, new_ranges, port):
     added = 0
     removed = 0
 
-    if len(group['IpPermissions']) > 0:
-        for permission in group['IpPermissions']:
-            if permission['FromPort'] <= port and permission['ToPort'] >= port :
-                old_prefixes = list()
-                to_revoke = list()
-                to_add = list()
-                for range in permission['IpRanges']:
-                    cidr = range['CidrIp']
-                    old_prefixes.append(cidr)
-                    if new_ranges.count(cidr) == 0:
-                        to_revoke.append(range)
-                        print(group['GroupId'] + ": Revoking " + cidr + ":" + str(permission['ToPort']))
+    perms = [
+        item for item in group['IpPermissions']
+        if item["FromPort"] <= port <= item["ToPort"]
+    ]
+    old_prefixes = []
+    old_prefixes_v6 = []
+    for perm in perms:
+        old_prefixes.extend([item["CidrIp"] for item in perm["IpRanges"]])
+        old_prefixes_v6.extend([
+            item["CidrIpv6"] for item in perm["Ipv6Ranges"]
+        ])
+    old_prefixes = set(old_prefixes)
+    old_prefixes_v6 = set(old_prefixes_v6)
+    to_revoke = old_prefixes - set(new_ranges["ipv4"])
+    to_revoke_6 = old_prefixes_v6 - set(new_ranges["ipv6"])
+    to_add = set(new_ranges["ipv4"]) - old_prefixes
+    to_add_6 = set(new_ranges["ipv6"]) - old_prefixes_v6
 
-                for range in new_ranges:
-                    if old_prefixes.count(range) == 0:
-                        to_add.append({ 'CidrIp': range })
-                        print(group['GroupId'] + ": Adding " + range + ":" + str(permission['ToPort']))
+    for perm in perms or [{
+        "ToPort": port, "FromPort": port, "IpProtocol": 'tcp'
+    }]:
+        print(("\n").join([
+            ("{}: Revoking {}:{}").format(
+                group['GroupId'], item, perm['ToPort']
+            ) for item in list(to_revoke | to_revoke_6)
+        ]))
+        removed += revoke_permissions(
+            client, group, perm, list(to_revoke), list(to_revoke_6)
+        )
+        print(("\n").join([
+            ("{}: Adding: {}:{}").format(
+                group['GroupId'], item, perm['ToPort']
+            ) for item in list(to_add | to_add_6)
+        ]))
+        added += add_permissions(
+            client, group, perm, list(to_add), list(to_add_6)
+        )
 
-                removed += revoke_permissions(client, group, permission, to_revoke)
-                added += add_permissions(client, group, permission, to_add)
-    else:
-        to_add = list()
-        for range in new_ranges:
-            to_add.append({ 'CidrIp': range })
-            print(group['GroupId'] + ": Adding " + range + ":" + str(port))
-        permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
-        added += add_permissions(client, group, permission, to_add)
-
-    print (group['GroupId'] + ": Added " + str(added) + ", Revoked " + str(removed))
+    print(("{}: Added {}, Revoked {}").format(
+        group['GroupId'], str(added), str(removed)
+    ))
     return (added > 0 or removed > 0)
 
-def revoke_permissions(client, group, permission, to_revoke):
+
+def revoke_permissions(client, group, permission, to_revoke, to_revoke_6):
     if len(to_revoke) > 0:
         revoke_params = {
             'ToPort': permission['ToPort'],
             'FromPort': permission['FromPort'],
-            'IpRanges': to_revoke,
+            'IpRanges': [{"CidrIp": ip} for ip in to_revoke],
+            'Ipv6Ranges': [{"CidrIpv6": ip} for ip in to_revoke_6],
             'IpProtocol': permission['IpProtocol']
         }
 
-        client.revoke_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[revoke_params])
+        client.revoke_security_group_ingress(
+            GroupId=group['GroupId'], IpPermissions=[revoke_params]
+        )
 
     return len(to_revoke)
 
-def add_permissions(client, group, permission, to_add):
+
+def add_permissions(client, group, permission, to_add, to_add_6):
     if len(to_add) > 0:
         add_params = {
             'ToPort': permission['ToPort'],
             'FromPort': permission['FromPort'],
-            'IpRanges': to_add,
+            'IpRanges': [{"CidrIp": ip} for ip in to_add],
+            'Ipv6Ranges': [{"CidrIpv6": ip} for ip in to_add_6],
             'IpProtocol': permission['IpProtocol']
         }
 
-        client.authorize_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[add_params])
+        client.authorize_security_group_ingress(
+            GroupId=group['GroupId'], IpPermissions=[add_params]
+        )
 
     return len(to_add)
 
+
 def get_security_groups_for_update(client, security_group_tag):
-    filters = list();
-    for key, value in security_group_tag.iteritems():
-        filters.extend(
-            [
-                { 'Name': "tag-key", 'Values': [ key ] },
-                { 'Name': "tag-value", 'Values': [ value ] }
-            ]
-        )
+    filters = []
+    for key, value in security_group_tag.items():
+        filters.extend([
+            {'Name': "tag-key", 'Values': [key]},
+            {'Name': "tag-value", 'Values': [value]}
+        ])
 
     response = client.describe_security_groups(Filters=filters)
 
     return response['SecurityGroups']
-
-'''
-Sample Event From SNS:
-
-{
-  "Records": [
-    {
-      "EventVersion": "1.0",
-      "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
-      "EventSource": "aws:sns",
-      "Sns": {
-        "SignatureVersion": "1",
-        "Timestamp": "1970-01-01T00:00:00.000Z",
-        "Signature": "EXAMPLE",
-        "SigningCertUrl": "EXAMPLE",
-        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
-        "Message": "{\"create-time\": \"yyyy-mm-ddThh:mm:ss+00:00\", \"synctoken\": \"0123456789\", \"md5\": \"45be1ba64fe83acb7ef247bccbc45704\", \"url\": \"https://ip-ranges.amazonaws.com/ip-ranges.json\"}",
-        "Type": "Notification",
-        "UnsubscribeUrl": "EXAMPLE",
-        "TopicArn": "arn:aws:sns:EXAMPLE",
-        "Subject": "TestInvoke"
-      }
-    }
-  ]
-}
-
-'''
